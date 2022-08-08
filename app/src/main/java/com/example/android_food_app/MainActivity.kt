1aefa8ea33d3290f4aa9a9b465d8fb7a7576db77
jyosharm8 <jyoti.sks@yahoo.com>
2022-08-08 18:48:47 +0530

    "Demo login with data binding without Authentication"

--- app/src/main/java/com/example/android_food_app/MainActivity.kt
+++ app/src/main/java/com/example/android_food_app/MainActivity.kt
@@ -2,10 +2,71 @@ package com.example.android_food_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.android_food_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.loginbutton.setOnClickListener{
+            startOrStopProgressBar()
+        }
+
+
     }
-}
+
+
+    private fun startOrStopProgressBar() {
+
+        binding.apply {
+            if (editTextPersonName.getText().length > 0 && editTextPassword.getText().length > 0) {
+               // String toastMessage = "Username: " + usernameEditText.getText().toString() + ", Password: " + passwordEditText.getText().toString();
+                Toast.makeText(getApplicationContext(), "correct email", Toast.LENGTH_SHORT).show();
+            } else {
+                //String toastMessage = "Username or Password are not populated";
+                Toast.makeText(getApplicationContext(), "incorrect email and password", Toast.LENGTH_SHORT).show();
+            }
+        }
+        }
+    }
+
+
+
+
+//b1.setOnClickListener(new View.OnClickListener() {
+//    @Override
+//    public void onClick(View v) {
+//        if(ed1.getText().toString().equals("admin") &&
+//            ed2.getText().toString().equals("admin")) {
+//            Toast.makeText(getApplicationContext(),
+//                "Redirecting...",Toast.LENGTH_SHORT).show();
+//        }else{
+//            Toast.makeText(getApplicationContext(), "Wrong
+//                    Credentials",Toast.LENGTH_SHORT).show();
+//
+//                    tx1.setVisibility(View.VISIBLE);
+//            tx1.setBackgroundColor(Color.RED);
+//            counter--;
+//            tx1.setText(Integer.toString(counter));
+//
+//            if (counter == 0) {
+//                b1.setEnabled(false);
+//            }
+//        }
+//    }
+//});
+//
+//b2.setOnClickListener(new View.OnClickListener() {
+//    @Override
+//    public void onClick(View v) {
+//        finish();
+//    }
+//});
+//}
+//}
